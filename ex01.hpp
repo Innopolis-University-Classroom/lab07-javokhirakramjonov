0847043a36bc0f88d6fca18ddd280733e598f84d
javokhirakramjonov <88347670+javokhirakramjonov@users.noreply.github.com>
2022-03-09 17:14:18 +0300

    Update ex01.hpp
    at13

--- ex01.hpp
+++ ex01.hpp
@@ -3,7 +3,7 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template <typename T>
-void multiples (int& sum, T x, int n)
+void multiples (T& sum, T x, int n)
 {
     sum = 1;
     for(int i = 1; i <= n; ++ i) {
